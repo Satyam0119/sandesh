6f64f42dcadacfd8022ad0e45ce301c37dd6fe70
Satyam0119 <118735981+Satyam0119@users.noreply.github.com>
2024-04-22 16:57:36 +0530

    add nextjs

---
+++ sandesh/app/(site)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="bg-green-500">
+        <p className="text-blue-600 text-3xl">Sandesh main page </p>
+      </div>
+      
+    );
+  }
